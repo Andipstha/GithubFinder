a9b77e0b306258123c977dc291d2d7a9f7a1017f
andipstha <sandip.stha120@gmail.com>
2023-02-24 23:28:09 +0545

    update All . #1

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
+//https://api.github.com/users/user_name/repos
+//
+
 const user_img = document.querySelector(".user_img");
 const userName = document.querySelector(".user_name h1");
 const followers_ = document.querySelector(".followers_ span");
 const follow_ = document.querySelector(".follow_ span");
 const repo_details = document.querySelector(".repo_details");
 const btn_submit = document.querySelector(".btn_submit");
+
 
 let user_name = '';
 
@@ -123,8 +127,7 @@ function fetchUser() {
                             `
                         );
                     })
-                    //I am taking maximum 6 repos
-                    // you can tak according to your requirement
+                    //taking maximum 6 repos
                     repo_details.innerHTML = repo_Data.slice(0, 6).join("");
 
                 }
@@ -132,3 +135,4 @@ function fetchUser() {
 
         });
 }
+
